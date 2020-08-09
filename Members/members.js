dfaf241984d2f70c33dc6fcd5feede7bb995e004
Stupid-Benz <66051526+Stupid-Benz@users.noreply.github.com>
2020-08-09 18:36:20 +0800

    Update Website

--- Members/members.js
+++ Members/members.js
@@ -1,6 +1,14 @@
 function toggle() {
-	var sec = document.getElementById('sec');
+	var sec1 = document.getElementById('sec1');
+	var sec2 = document.getElementById('sec2');
+	var sec3 = document.getElementById('sec3');
+	var sec4 = document.getElementById('sec4');
+	var sec5 = document.getElementById('sec5');
 	var nav = document.getElementById('navigation');
-	sec.classList.toggle('active');
+	sec1.classList.toggle('active');
+	sec2.classList.toggle('active');
+	sec3.classList.toggle('active');
+	sec4.classList.toggle('active');
+	sec5.classList.toggle('active');
 	nav.classList.toggle('active');
 }
